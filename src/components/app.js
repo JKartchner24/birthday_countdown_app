716b08fd96dfc67558e33168bd6025bd80873f49
Jake <kartchner56jake@gmail.com>
2018-04-02 14:20:33 -0600

    initial commit:

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BirthdayForm />
+      </div>
+    );
+  }
+}
