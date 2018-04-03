d080cec696b7b54cdce3d04f4d41fae037035b57
Jake <kartchner56jake@gmail.com>
2018-04-03 11:18:48 -0600

    added time calculation

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,61 +1,60 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import Clock from './clock';
+import Clock from './Clock';
 
 class BirthdayForm extends Component {
 
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleGenerate = this.handleGenerate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleGenerate = this.handleGenerate.bind(this)
 
-		this.state = {
-			startDate: moment(),
-			formCompleted: false
-		}
+        this.state = {
+            startDate: moment(),
+            formCompleted: false
+        }
 
-	}
+    }
 
-	handleChange(date) {
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
 
-		this,setState({
-			startDate: date
-		});
-	}
+    handleGenerate() {
+        this.setState({
+            formCompleted: true
+        })
+    }
 
-	handleGenerate() {
-		this.setState({
-			formCompleted: true
-		})
-	}
+    render() {
+        return (
+            <div>
+            {
+                this.state.formCompleted ?
+                
+                    <div>
+                        <Clock birthdayFormState={this.state}/>
+                    </div>
+                :
+                    <div>
+                        <h1>Birthday Form Component!</h1>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                        />
+                        <a onClick={this.handleGenerate}>Generate Countdown</a>
+                    </div>
 
-	render() {
+            }
+                
 
-		return (
-			<div>
-			{
-				this.state.formCompleted ? 
-
-					 <div>
-					 	<Clock birthdayFormState={this.state}/>
-					 </div>
-				:
-					 <div>
-					 	<h1>Birthday Form Component!</h1>
-						<DatePicker 
-							selected={this.state.startDate}
-							onChange={this.handleChange}
-						/>
-						<a onClick={this.handleGenerate}>Generate Countdown</a>
-					 </div>
-
-				
-			}
-			</div>
-		)
-	}
+            </div>
+        )
+    }
 }
 
 export default BirthdayForm;
